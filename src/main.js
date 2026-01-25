306c4583db019c6f2013ee2ec3350196b134ba77
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2026-01-25 06:05:11 +0530

    file setup

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Phaser from "./lib/phaser.js" // need to mention file name , refernce to Phaser object in window
+
+// game instance
+const game = new Phaser.Game({
+    parent:"gamne-container"
+})
+
